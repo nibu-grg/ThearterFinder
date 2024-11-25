231f5023b0eec0ad074e246d8f5558bf910ecc9b
nibu-grg <20047500@mydbs.ie>
2024-11-26 02:48:28 +0530

    Added validation on db connection and get data

--- index.js
+++ index.js
@@ -30,48 +30,52 @@ app.get('/theaters', (request, response) => {
     const connection = mysql.createConnection(mysqlConnection);
     const query = 'SELECT * FROM Theater';
     connection.connect((err) => {
+        if (err) {
+            console.error('Database connection error:', err);
+            return response.status(500).json({ error: 'Database connection error' });
+        }
+
         connection.query(query, (err, results) => {
+            if (err) {
+                console.error('Error executing query:', err);
+                return response.status(500).json({ error: 'Query execution error' });
+            }
+
             response.status(200).json(results);
         });
     });
-
 });
 
-app.get('/search', (req, res, next) => {
+app.get('/search', (req, res) => {
     const connection = mysql.createConnection(mysqlConnection);
-    const searchTerm = req.query.theatername;
+    const searchTerm = req.query.EirCode;
+
     if (!searchTerm) {
-        return res.status(400)
-            .json(
-                {
-                    error: 'Theater name is required'
-                }
-            );
+        return res.status(400).json({ error: 'Eircode is required' });
     }
- 
+
     const query = `
-    SELECT * FROM Theater
-    WHERE Theater_Name LIKE ?
-  `;
- 
+        SELECT * FROM Theater
+        WHERE EirCode LIKE ?
+    `;
+
     const searchValue = `%${searchTerm}%`;
- 
-    connection.query(query, [searchValue, searchValue],
-        (err, results) => {
-            if (err) {
-                console
-                    .error('Error executing search query:', err);
-                return res.status(500)
-                    .json(
-                        {
-                            error: 'Internal server error'
-                        });
-            }
- 
-            res.json(results);
-        });
+
+    connection.query(query, [searchValue], (err, results) => {
+        if (err) {
+            console.error('Error executing search query:', err);
+            return res.status(500).json({ error: 'Internal server error' });
+        }
+        if (results.length > 0) {
+            return res.status(200).json(results); 
+        } else {
+            return res.status(404).json({ message: 'No theaters found for this Eircode' });
+        }
+    });
 });
 
+  
+  
 
 const PORT = 3000;
 app.listen(PORT, () => {
