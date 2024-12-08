9fd4d1da0fd243151bc7666c161270c51c328d1d
nibu-grg <20047500@mydbs.ie>
2024-12-09 03:03:06 +0530

    Test for PUT API

--- test/index.test.js
+++ test/index.test.js
@@ -4,9 +4,9 @@ const app = require("../index.js");
 
 chai.should();
 chai.use(chaiHttp);
-
+const theaterId = 38;
 describe('Task API', () => {
-    describe("GET /api/theaters", () => {
+    describe("GET /theaters", () => {
         it("Should perform GET", (done) => {
             chai.request(app)
                 .get("/theaters")
@@ -21,10 +21,10 @@ describe('Task API', () => {
     describe('POST /addTheater', () => {
         it('should add a new theater', (done) => {
             const newTheater = {
-                Theater_Name: 'New Theater',
-                Location: 'Some Location',
-                City: 'Some City',
-                EirCode: '12345',
+                Theater_Name: 'Test New Theater',
+                Location: 'Dublin 11',
+                City: 'City',
+                EirCode: 'D11FK64',
                 Mobile: 1234567890,
                 Email: 'newtheater@example.com'
             };
@@ -39,6 +39,28 @@ describe('Task API', () => {
                 });
         });
     });
+
+    describe('PUT /updateTheater/:Theater_Id', () => {
+        it('should update an existing theater', (done) => {
+            const updatedTheater = {
+                Theater_Name: 'Test Updated Theater',
+                Location: 'Dublin',
+                City: 'City Center',
+                EirCode: 'D01AX23',
+                Mobile: 9876543210,
+                Email: 'updatedtheater@example.com'
+            };
+            chai.request(app)
+                .put(`/updateTheater/${theaterId}`)  
+                .send(updatedTheater)              
+                .end((err, res) => {
+                    res.should.have.status(200);   
+                    res.body.should.have.property('message').eql('Theater updated successfully'); 
+                    done(); 
+                });
+        });
+    });
+    
 
 });
 
